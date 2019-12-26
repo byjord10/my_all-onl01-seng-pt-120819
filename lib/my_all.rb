cb639bb2f9623c4b9dbd65b0288000faaa4e226e
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-26 17:59:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,9 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-
+  if block_return_values.include?(false)
+    return false
+  else
+    true
+  end 
 end
